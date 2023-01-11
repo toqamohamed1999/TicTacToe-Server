92af546d8edb8d0457e7689fd3d10d2d533345e9
toqamohamed1999 <toqa111999mohamed@gmail.com>
2023-01-11 08:37:22 +0200

    modify online user

--- src/tictactoe/server/ServerHandler.java
+++ src/tictactoe/server/ServerHandler.java
@@ -8,11 +8,14 @@ package tictactoe.server;
 import Database.DatabaseConnection;
 import Database.DatabaseOperations;
 import Logic.ServerHandlerLogic;
+import Logic.User;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Vector;
 
 /**
@@ -42,6 +45,7 @@ public class ServerHandler {
     void stopServer() {
         try {
             serverSocket.close();
+            serverSocket = null;
         } catch (IOException ex) {
             ex.printStackTrace();
         }
@@ -58,6 +62,7 @@ class Handler extends Thread {
     ServerHandlerLogic serverHandlerLogic;
     String[] operation = null;
     DatabaseOperations databaseOperations;
+    ArrayList<User> usersList;
 
     public Handler(Socket socket) {
         serverHandlerLogic = new ServerHandlerLogic();
@@ -103,12 +108,12 @@ class Handler extends Thread {
     }
 
     void doAction() {
-        int index = getClientIndex();
-        System.out.println("index client = " + index);
         if (operation[0].equals("signIn")) {
+            int index = getClientIndex();
             System.out.println("siginINNNNNNNNNNNNNNNNNNNNNN");
             if (serverHandlerLogic.checkUserExist()) {
                 clientsVector.get(index).ps.println("signInVerified");
+                serverHandlerLogic.addUserToOnlineList();
             } else {
                 clientsVector.get(index).ps.println("signInNotVerified");
             }
@@ -116,6 +121,7 @@ class Handler extends Thread {
             sendAllOnlineUsers();
         }
         if (operation[0].equals("SignUp")) {
+            int index = getClientIndex();
             clientsVector.get(index).ps.println("*********signUpVerified");
             index = getClientIndex();
             if (serverHandlerLogic.checksignUp() == true) {
@@ -124,12 +130,17 @@ class Handler extends Thread {
                 clientsVector.get(index).ps.println("signUpNotVerified");
             }
         }
+
+        if (operation[0].equals("sendRequest")) {
+            int  index = getClientIndex(operation[2]);
+            clientsVector.get(index).ps.println("recieveRequest,"+operation[1]);
+        }
     }
 
     int getClientIndex() {
-
         for (int i = 0; i < clientsVector.size(); i++) {
-            System.out.println("ip ser=" + socket.getInetAddress().getHostAddress());
+            System.out.println("ip ser=" + this.socket.getInetAddress().getHostAddress());
+            System.out.println("operation = " + Arrays.toString(operation));
             if (operation[1].equals(socket.getInetAddress().getHostAddress())) {
                 System.out.println("trueeeeeeeeeeeeeeeeeeeeeeeeeeee");
                 return i;
@@ -139,12 +150,26 @@ class Handler extends Thread {
         return -1;
     }
 
-    void sendAllOnlineUsers() {
-
+    int getClientIndex(String ip) {
         for (int i = 0; i < clientsVector.size(); i++) {
+          //  System.out.println("ip ser=" + this.socket.getInetAddress().getHostAddress());
+            System.out.println("operationnn = " + Arrays.toString(operation));
+            if (ip.equals(socket.getInetAddress().getHostAddress())) {
+                System.out.println("trueeeeeeeeeeeeeeeeeeeeeeeeeeee");
+                return i;
+            }
+        }
+        return -1;
+    }
+    void sendAllOnlineUsers() {
+        usersList = serverHandlerLogic.getOnlineListUsers();
+        for (int i = 0; i < usersList.size(); i++) {
             String ip = clientsVector.get(i).socket.getInetAddress().getHostAddress();
-            System.out.println("ip ser=" + ip);
-            ps.println("sendAllUsers," + ip);
+            User user = usersList.get(i);
+            // System.out.println("mmmmmmmmm" + " ip ser=" + ip);
+            // System.out.println("mmmmmmmmmmmmmmmmmmmmmm" + user.toString());
+            //  ps.println("sendAllUsers," + ip);
+            ps.println("sendAllUsers," + ip + "," + user.getUserName() + "," + user.getEmail() + "," + user.getGender());
         }
     }
 
